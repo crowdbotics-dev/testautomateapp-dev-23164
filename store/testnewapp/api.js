4dd1fd0fd87a16a81378d83ef9c5ca1df64b5f1d
Crowdbotics <engineering@crowdbotics.com>
2021-11-26 19:39:37 +0000

    Update store.

--- store/testnewapp/api.js
+++ store/testnewapp/api.js
@@ -1,11 +1,7 @@
 import axios from "axios"
-import {
-  TEST_NEW_APP_USERNAME,
-  TEST_NEW_APP_PASSWORD
-} from "react-native-dotenv"
+import {} from "react-native-dotenv"
 const testnewapp = axios.create({
   baseURL: "https://crowdbotics-slack-dev.herokuapp.com/admin/",
-  auth: { username: TEST_NEW_APP_USERNAME, password: TEST_NEW_APP_PASSWORD },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 export const apiService = {}
